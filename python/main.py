fed40fcfb97af8b777fa09acd0834a2f19b5ae9c
b3ella <goliveirapk@gmail.com>
2023-11-22 11:45:35 -0300

    feat: adds function login

--- python/main.py
+++ python/main.py
@@ -1,3 +1,4 @@
+from instagram_private_api import Client
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,3 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+def login(username: str, password: str) -> Client:
+    return Client(username, password)
