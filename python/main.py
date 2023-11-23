1262e5ca9b8c2dc1752740c6d40b03f4dbcb5685
b3ella <goliveirapk@gmail.com>
2023-11-23 17:28:02 -0300

    feat: adds function that will render the login

--- python/main.py
+++ python/main.py
@@ -1,5 +1,5 @@
 from instagram_private_api import Client
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,16 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/login")
+def handle_login():
+    content_type = request.headers.get('Content-Type')
+    if (content_type != 'application/json'):
+        return "<p>content type not suported</p>"
+    username = request.json.get("username")
+    password = request.json.get("password")
+    print(username, password)
 
 
 def login(username: str, password: str) -> Client:
